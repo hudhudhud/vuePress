d972a5f77d6cdfbd9b8dd2476894ccf2a4a68269
hudan ys3032(CW) <ys.hudana@h3c.com>
2022-09-19 10:34:52 +0800

    add

---
+++ docs/.vuepress/components/MyTest.vue
@@ -0,0 +1,21 @@
+<template>
+<section>
+    <h3>{{txt}}</h3>
+    <slot></slot>
+</section>
+</template>
+<script>
+export default {
+    name:'MyTest',
+    data(){
+        return {
+            txt:'This is customer component.'
+        }
+    }
+}
+</script>
+<style>
+section{
+    border:1px solid pink;
+}
+</style>
