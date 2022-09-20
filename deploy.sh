cbd1e5a15aeb66cd4114b119c78097627d994677
hudan ys3032(CW) <ys.hudana@h3c.com>
2022-09-20 16:25:58 +0800

    edit

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,14 @@ npm run build
 cd docs/.vuepress/dist
 
 # 创建git的本地仓库，提交修改
-git init
+# git init
 git add -A
 git commit -m 'deploy'
-
+# git config http.proxy http://proxy02.h3c.com:8080
+# git config https.proxy http://proxy02.h3c.com:8080
+# git remote add origin https://github.com/hudhudhud/hudhudhud.github.io.git
 # 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
 # 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
-git push -f git@github.com:hudhudhud/hudhudhud.github.io.git master
-
+#git push -f git@github.com:hudhudhud/hudhudhud.github.io.git master
+git push --set-upstream origin master
 cd -
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 ---
-home: false
+home: true
 heroImage: /egg.png
 heroText: log
 tagline: 一点一滴都是进步
 actionText: 马上进入 →
-actionLink: /pages/folder1/test1.md
+actionLink: /pages/flowFormAPI.md
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
